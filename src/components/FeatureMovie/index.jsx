ff186b0c378358d6e7472854bccf5b5b59fe6582
matheus souza <ruivorobado@gmail.com>
2021-09-30 18:56:31 -0300

    sessao com o filme recomendado completa

--- src/components/FeatureMovie/index.jsx
+++ src/components/FeatureMovie/index.jsx
@@ -3,6 +3,12 @@ import { FeatureMovieStyled } from './style'
 
 
 export default function FeatureMovie({item}) {
+  let firstDate = new Date(item.first_air_date)
+  let genres = []
+  for(let i in item.genres){
+    genres.push(item.genres[i].name)
+  }
+
   return (
     <FeatureMovieStyled
     style={{
@@ -12,7 +18,24 @@ export default function FeatureMovie({item}) {
     }}
     >
       <div className="featured--vertical">
+        <div className="featured--horizontal">
+          <div className="featured--name">{item.original_name}</div>
+         
+          <div className="featured--info">
+            <div className="featured--points display-block-and-margin">{item.vote_average} pontos</div>
+            <div className="featured--year display-block-and-margin">{firstDate.getFullYear()}</div>
+            <div className="featured--seasons display-block-and-margin">{item.number_of_seasons} {item.number_of_seasons === 1? <>temporada</>: <>temporadas</>}</div>
+          </div>
+         
+          <div className="featured--description">{item.overview}</div>
         
+          <div className="feature--buttons">
+            <a className="featured--watchbutton btn" href={`/watch/${item.id}`}>Assistir</a>
+            <a className="featured--mylistbutton btn" href={`/list/add/${item.id}`}>+ Minha Lista</a>
+          </div>
+        
+          <div className="featured--genres">Gêneros <strong>{genres.join(', ')}</strong></div>
+        </div>
       </div>
 
     </FeatureMovieStyled>
--- src/components/FeatureMovie/style.js
+++ src/components/FeatureMovie/style.js
@@ -5,6 +5,67 @@ export const FeatureMovieStyled = styled.section`
   .featured--vertical{
     width: inherit;
     height: inherit;
-    background: linear-gradient(to top, #111, transparent);
+    background: linear-gradient(to top, #111 10%, transparent 90%);
+    .featured--horizontal{
+      width: inherit;
+      height: inherit;
+      background: linear-gradient(to right, #111 30%, transparent 70%);
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      padding-left: 30px;
+      .featured--name{
+        font-size: 60px;
+        font-weight: bold;
+      }
+      .featured--info{
+          font-size:18px;
+          font-weight: bold;
+          margin-top: 15px;
+          .featured--points{
+            color: #46d369;
+          }
+          .display-block-and-margin{
+            display: inline-block;
+            margin-right:15px; 
+          }
+      }
+      .featured--description{
+        margin-top: 15px;
+        font-size: 20px;
+        color: #999;
+        max-width: 40%;
+      }
+      .feature--buttons{
+        margin-top: 15px;
+        .btn{
+          display: inline-block;
+          font-size: 20px;
+          font-weight: bold;
+          padding: 12px 25px;
+          border-radius: 5px;
+          text-decoration: none;
+          margin-right: 10px;
+          opacity: 1;
+          transition: all ease .4s;
+          &:hover{
+            opacity: 0.7;
+          }
+        }
+        .featured--watchbutton{
+          background-color: #fff;
+          color: #000;
+        }
+        .featured--mylistbutton{
+          background-color: #333;
+          color: #fff;
+        }
+      }
+      .featured--genres{
+        margin-top: 15px;
+        font-size: 14px;
+        color: #999;
+      }
+    }
   }
 `;
