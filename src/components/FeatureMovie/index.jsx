dc95cf3bba58426446f6c66d3c0c7bc986f089c7
matheus souza <ruivorobado@gmail.com>
2021-09-30 17:31:04 -0300

    efeito de cores lineares adicionados a imagem

--- src/components/FeatureMovie/index.jsx
+++ src/components/FeatureMovie/index.jsx
@@ -11,7 +11,9 @@ export default function FeatureMovie({item}) {
       backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
     }}
     >
-      <div>{item.original_name}</div>
+      <div className="featured--vertical">
+        
+      </div>
 
     </FeatureMovieStyled>
   )
--- src/components/FeatureMovie/style.js
+++ src/components/FeatureMovie/style.js
@@ -2,5 +2,9 @@ import styled from 'styled-components';
 
 export const FeatureMovieStyled = styled.section`
   height: 100vh;
-
+  .featured--vertical{
+    width: inherit;
+    height: inherit;
+    background: linear-gradient(to top, #111, transparent);
+  }
 `;
